f6b4ccaf4862ad96e0e956c90d2e3de8dc296763
Tsingwaa <zengchh3@gmail.com>
2021-09-26 09:46:39 +0800

    new exp

--- scripts/baseline/train.sh
+++ scripts/baseline/train.sh
@@ -6,4 +6,4 @@ export PYTHONPATH=$PYTHONPATH:/home/waa/Projects/classification
 #     train.py --config_file "configs/train_v1.0_20210827.yaml"
 
 # Single-GPU Training
-python3 train.py --local_rank -1 --config_fpath 'configs/train_20210925_baseline.yaml'
+python3 train.py --local_rank -1 --config_fpath 'configs/train_20210926_baseline2.yaml'
