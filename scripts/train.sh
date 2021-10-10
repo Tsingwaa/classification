1c40373401acafcbc9a5acad40a536c05b540de2
Tsingwaa <zengchh3@gmail.com>
2021-10-10 23:00:18 +0800

    reformat configs

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,4 +6,4 @@ export PYTHONPATH=$PYTHONPATH:/home/waa/Projects/classification
 #     train.py --config_file "configs/train_v1.0_20210827.yaml"
 
 # Single-GPU Training
-CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1 --config_fpath 'configs/20211007_adapt_randaug.yaml'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1 --config_fpath 'configs/CIFAR10/20211010_CIFAR10_randaug.yaml'
