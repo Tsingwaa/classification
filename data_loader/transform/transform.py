3843ffae1afe7faee1a69a9a96497fa9e9841e7b
Tsingwaa <zengchh3@gmail.com>
2021-10-17 23:18:05 +0800

    update BaseTransform

--- data_loader/transform/transform.py
+++ data_loader/transform/transform.py
@@ -7,38 +7,39 @@ IN_MEAN = [0.485, 0.456, 0.406]
 IN_STD = [0.229, 0.224, 0.225]
 
 
-def base_transform(phase='train', resize=(224, 224),
-                   mean=None, std=None, **kwargs):
-    if phase == 'train':
-        # transforms.RandomOrder
-        ret_transform = transforms.Compose([
-            transforms.Resize(size=(int(resize[0] / 0.875),
-                                    int(resize[1] / 0.875))),
-            transforms.RandomCrop(resize),
-            transforms.RandomHorizontalFlip(0.5),
-            transforms.RandomRotation(25),
-            transforms.ColorJitter(brightness=0.126, saturation=0.5),
-            transforms.ToTensor(),
-            transforms.Normalize(mean, std),
-            transforms.RandomErasing()
-        ])
-    else:
-        ret_transform = transforms.Compose([
-            transforms.Resize(size=(int(resize[0]),
-                                    int(resize[1]))),
-            # transforms.CenterCrop(resize),
-            transforms.ToTensor(),
-            transforms.Normalize(mean, std)
-        ])
-    return ret_transform
+class BaseTransform:
+    def __init__(self, phase='train', resize=(224, 224),
+                 **kwargs):
+        self.phase = phase
+        self.resize = resize
+
+    def __call__(self, x, mean=IN_MEAN, std=IN_STD):
+        if self.phase == 'train':
+            ret_transform = transforms.Compose([
+                transforms.Resize(int(self.resize[1] / 0.875)),
+                transforms.RandomCrop(self.resize),
+                transforms.RandomHorizontalFlip(0.5),
+                # transforms.RandomRotation(25),
+                # transforms.ColorJitter(brightness=0.126, saturation=0.5),
+                transforms.ToTensor(),
+                transforms.Normalize(mean, std),
+                # transforms.RandomErasing()
+            ])
+        else:
+            ret_transform = transforms.Compose([
+                transforms.Resize(self.resize),
+                # transforms.CenterCrop(resize),
+                transforms.ToTensor(),
+                transforms.Normalize(mean, std)
+            ])
+        return ret_transform
 
 
 def flower_transform(resize=(224, 224), phase='train',
                      mean=None, std=None, **kwargs):
     if phase == 'train':
         ret_transform = transforms.Compose([
-            transforms.Resize(size=(int(resize[0] / 0.875),
-                                    int(resize[1] / 0.875))),
+            transforms.Resize(int(resize[1] / 0.875)),
             transforms.RandomCrop(resize),
             transforms.RandomHorizontalFlip(0.5),
             transforms.RandomRotation(25),
@@ -136,35 +137,6 @@ def rand_transform(phase='train', resize=(224, 224),
         ])
     return ret_transform
 
-# class RandTransform:
-#     def __init__(self, phase='train', resize=(224, 224),
-#                  **kwargs):
-#         self.phase = phase
-#         self.resize = resize
-#         self.n = kwargs.get('rand_n', 2)
-#         self.m = kwargs.get('rand_m', 10)
-
-#     def __call__(self, x, percent=0,
-#                  mean=IN_MEAN, std=IN_STD):
-#         if self.phase == 'train':
-#             ret_transform = transforms.Compose([
-#                 transforms.Resize(size=(int(self.resize[0] / 0.875),
-#                                         int(self.resize[1] / 0.8))),
-#                 transforms.RandomCrop(self.resize),
-#                 RandAugment(self.n, self.m, percent),
-#                 transforms.ToTensor(),
-#                 transforms.Normalize(mean, std),
-#             ])
-#         else:
-#             ret_transform = transforms.Compose([
-#                 transforms.Resize(size=(int(self.resize[0]),
-#                                         int(self.resize[1]))),
-#                 transforms.ToTensor(),
-#                 transforms.Normalize(mean, std)
-#             ])
-
-#         return ret_transform(x)
-
 
 # from ildoonet/pytorch-randaugment
 class RandTransform:
@@ -196,8 +168,7 @@ class RandTransform:
             ])
         else:
             ret_transform = transforms.Compose([
-                transforms.Resize(size=(int(self.resize[0]),
-                                        int(self.resize[1]))),
+                transforms.Resize(self.resize),
                 transforms.ToTensor(),
                 transforms.Normalize(mean, std)
             ])
