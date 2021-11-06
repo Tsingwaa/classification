38d7d3712ad0349696b8bf26c138f597598cfccf
Tsingwaa <zengchh3@gmail.com>
2021-11-06 17:49:42 +0800

    update

--- scripts/adv/train.sh
+++ scripts/adv/train.sh
@@ -15,4 +15,4 @@ export PYTHONPATH=$PYTHONPATH:$HOME/Projects/classification
 
 # Single-GPU Training
 CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1 --config_path \
-        'configs/miniImageNet20_0.05/20211106_r18_LinfPGD_joint0.8_dualBN1.yaml'
+        'configs/miniImageNet20_0.05/20211106_r18_LinfPGD_joint0.5_RS_dualBN.yaml'
