3589de43dbdb3dd132d29b48548c79bcf8722aba
Tsingwaa.31 <zengchh3@gmail.com>
2021-11-11 22:06:27 +0800

    modify config folder name

--- scripts/adv/eval.sh
+++ scripts/adv/eval.sh
@@ -12,7 +12,7 @@ export PYTHONPATH=$PYTHONPATH:$HOME/Projects/classification/
 # CUDA_VISIBLE_DEVICES=0 python3 eval.py --config_path 'configs/CIFAR10_0.01/20211014_CIFAR10_0.01_reverse_adapt_randaug_m15n2.yaml'
 
 CUDA_VISIBLE_DEVICES=0 python3 eval.py --local_rank -1 --config_path \
-        'configs/miniImageNet20_0.05/20211108_r18_LinfPGD_joint0.8_dualBN_adapt_epsilon.yaml'
+        'configs/miniIN20_0.05/20211109_r18_resample_LinfPGD_joint0.8_dualBN_adapt_epsilon.yaml'
 # CUDA_VISIBLE_DEVICES=0 python3 eval.py --local_rank -1 --config_path \
 #         'configs/miniImageNet_0.05/20211024_resnet18_oversample.yaml'
 # CUDA_VISIBLE_DEVICES=0 python3 eval.py --local_rank -1 --config_path \
---
+++ scripts/self-sp/train_onlyssp.py
@@ -0,0 +1,321 @@
+"""trainer script """
+import random
+import warnings
+import argparse
+import yaml
+import numpy as np
+import torch
+# from pudb import set_trace
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from sklearn import metrics
+from prefetch_generator import BackgroundGenerator
+# Distribute Package
+from apex import amp
+from torch.nn.parallel import DistributedDataParallel
+# Custom Package
+from base.base_trainer import BaseTrainer
+from utils import AccAverageMeter, rotation
+
+
+class DataLoaderX(DataLoader):
+    def __iter__(self):
+        return BackgroundGenerator(super().__iter__(), max_prefetch=8)
+
+
+class Trainer(BaseTrainer):
+    def __init__(self, local_rank=None, config=None):
+        super(Trainer, self).__init__(local_rank, config)
+        self.sp_weight_scheduler = self.network_param['sp_weight_scheduler']
+
+    def train(self):
+        #######################################################################
+        # Initialize Dataset and Dataloader
+        #######################################################################
+        # set_trace()
+        train_transform = self.init_transform(self.train_transform_config)
+        trainset = self.init_dataset(self.trainset_config, train_transform)
+        train_sampler = self.init_sampler(trainset)
+
+        self.trainloader = DataLoaderX(
+            trainset,
+            batch_size=self.train_batch_size,
+            shuffle=(train_sampler is None),
+            num_workers=self.train_num_workers,
+            pin_memory=True,
+            drop_last=True,
+            sampler=train_sampler
+        )
+
+        if self.local_rank != -1:
+            print(f'global_rank {self.global_rank}/{self.world_size},'
+                  f'local_rank {self.local_rank},'
+                  f'sampler "{self.train_sampler_name}"')
+
+        if self.local_rank in [-1, 0]:
+            val_transform = self.init_transform(self.val_transform_config)
+            valset = self.init_dataset(self.valset_config, val_transform)
+            self.valloader = DataLoaderX(
+                valset,
+                batch_size=self.val_batch_size,
+                shuffle=False,
+                num_workers=self.val_num_workers,
+                pin_memory=True,
+                drop_last=False,
+            )
+
+        #######################################################################
+        # Initialize Network
+        #######################################################################
+        self.model = self.init_model()
+
+        #######################################################################
+        # Initialize Loss
+        #######################################################################
+        self.criterion = self.init_loss()
+
+        #######################################################################
+        # Initialize Optimizer
+        #######################################################################
+        self.optimizer = self.init_optimizer()
+
+        #######################################################################
+        # Initialize DistributedDataParallel
+        #######################################################################
+        if self.local_rank != -1:
+            self.model, self.optimizer = amp.initialize(
+                self.model,
+                self.optimizer,
+                opt_level='O1'
+            )
+            self.model = DistributedDataParallel(
+                self.model,
+                device_ids=[self.local_rank],
+                output_device=self.local_rank,
+                find_unused_parameters=True
+            )
+        #######################################################################
+        # Initialize LR Scheduler
+        #######################################################################
+        self.lr_scheduler = self.init_lr_scheduler()
+
+        #######################################################################
+        # Start Training
+        #######################################################################
+        for epoch in range(self.start_epoch, self.total_epochs + 1):
+            # learning rate decay by epoch
+            if self.lr_scheduler_mode == 'epoch':
+                self.lr_scheduler.step()
+
+            if self.local_rank != -1:  # 多卡同步sampler生成的索引块
+                train_sampler.set_epoch(epoch)
+
+            train_mr, train_loss = self.train_epoch(epoch)
+
+            if self.local_rank in [-1, 0]:
+                val_mr, val_recalls, val_loss = self.evaluate(epoch)
+
+                self.logger.debug(
+                    'Epoch[{epoch:>3d}/{total_epochs}] '
+                    'Trainset Loss={train_loss:.4f} SP={sp_loss:.4f} '
+                    'SSP={ssp_loss:.4f} MR={train_mr:.2%} || '
+                    'Valset Loss={val_loss:.4f} MR={val_mr:.2%}'.format(
+                        epoch=epoch,
+                        total_epochs=self.total_epochs,
+                        train_loss=train_loss['total'],
+                        sp_loss=train_loss['sp'],
+                        ssp_loss=train_loss['ssp'],
+                        train_mr=train_mr,
+                        val_loss=val_loss,
+                        val_mr=val_mr,
+                    )
+                )
+
+                if len(val_recalls) <= 20 and epoch == self.total_epochs:
+                    self.logger.info("Class recalls:{val_recalls}\n\n")
+
+                # Save log by tensorboard
+                self.writer.add_scalar(f'{self.exp_name}/LearningRate',
+                                       self.optimizer.param_groups[0]['lr'],
+                                       epoch)
+                self.writer.add_scalars(f'{self.exp_name}/Loss',
+                                        {'train_loss': train_loss['total'],
+                                         'sp_loss': train_loss['sp'],
+                                         'ssp_loss': train_loss['ssp'],
+                                         'val_loss': val_loss}, epoch)
+                self.writer.add_scalars(f'{self.exp_name}/Recall',
+                                        {'train_mr': train_mr,
+                                         'val_mr': val_mr}, epoch)
+
+                self.save_checkpoint(epoch, val_mr, val_recalls)
+
+        if self.local_rank in [-1, 0]:
+            self.logger.info(
+                f"===> Result directory: '{self.save_dir}'\n"
+                f"*********************************************************"
+                f"*********************************************************"
+            )
+
+    def train_epoch(self, epoch):
+        self.model.train()
+
+        train_pbar = tqdm(
+            total=len(self.trainloader),
+            desc='Train Epoch[{:>3d}/{}]'.format(epoch, self.total_epochs)
+        )
+
+        all_labels = []
+        all_preds = []
+        train_loss_meter = AccAverageMeter()
+        sp_loss_meter = AccAverageMeter()
+        ssp_loss_meter = AccAverageMeter()
+        for i, (batch_imgs, batch_labels) in enumerate(self.trainloader):
+            if self.lr_scheduler_mode == 'iteration':
+                self.lr_scheduler.step()
+
+            self.optimizer.zero_grad()
+            # Self supervised learning
+            # Step 1: generate rotated samples and labels
+            batch_selfsp_imgs, batch_selfsp_labels = rotation(batch_imgs)
+
+            batch_selfsp_imgs = batch_selfsp_imgs.cuda()
+            batch_selfsp_labels = batch_selfsp_labels.cuda()
+            batch_imgs = batch_imgs.cuda()
+            batch_labels = batch_labels.cuda()
+
+            # Step 2: train with rotated imgs
+            batch_feat_map = self.model(batch_imgs, output_type='feat_map')
+            # truncate gradient and add a new projector to keep learning with
+            # classifier.
+            batch_feat_map1 = self.model.projector(batch_feat_map.detach())
+            batch_feat_vec = self.model.avgpool(batch_feat_map1)
+            batch_feat_vec = torch.flatten(batch_feat_vec, 1)
+            batch_prob = self.model.fc(batch_feat_vec)
+
+            batch_selfsp_prob = self.model(batch_selfsp_imgs,
+                                           output_type='ssp')
+
+            sp_loss = self.criterion(batch_prob, batch_labels)
+            selfsp_loss = self.criterion(batch_selfsp_prob,
+                                         batch_selfsp_labels)
+
+            if self.sp_weight_scheduler == 'progressive':
+                # Startly, mainly use ssp; then use supervision progressively.
+                sp_weight = epoch / self.total_epochs
+            else:
+                sp_weight = 0.5
+            total_loss = sp_weight * sp_loss + (1 - sp_weight) * selfsp_loss
+
+            if self.local_rank != -1:
+                with amp.scale_loss(total_loss, self.optimizer) as scaled_loss:
+                    scaled_loss.backward()
+                self.optimizer.step()
+                self._reduce_loss(total_loss)
+            else:
+                total_loss.backward()
+                self.optimizer.step()
+
+            batch_pred = batch_prob.max(1)[1]
+            train_loss_meter.update(total_loss.item(), 1)
+            sp_loss_meter.update(sp_loss.item(), 1)
+            ssp_loss_meter.update(selfsp_loss.item(), 1)
+
+            all_labels.extend(batch_labels.cpu().numpy().tolist())
+            all_preds.extend(batch_pred.cpu().numpy().tolist())
+
+            if self.local_rank in [-1, 0]:
+                train_pbar.update()
+                train_pbar.set_postfix_str(
+                    'LR:{:.1e} Loss:{:.2f} SP:{:.2f} SSP:{:.2f}'.format(
+                        self.optimizer.param_groups[0]['lr'],
+                        train_loss_meter.avg,
+                        sp_loss_meter.avg,
+                        ssp_loss_meter.avg,
+                    )
+                )
+
+        train_mr = metrics.balanced_accuracy_score(all_labels, all_preds)
+        train_pbar.set_postfix_str(
+            'LR:{:.1e} Loss:{:.2f} SP:{:.2f} SSP:{:.2f} MR:{:.2%}'.format(
+                self.optimizer.param_groups[0]['lr'],
+                train_loss_meter.avg,
+                sp_loss_meter.avg,
+                ssp_loss_meter.avg,
+                train_mr))
+        train_pbar.close()
+
+        train_loss_dict = {
+            'total': train_loss_meter.avg,
+            'sp': sp_loss_meter.avg,
+            'ssp': ssp_loss_meter.avg,
+        }
+
+        return train_mr, train_loss_dict
+
+    def evaluate(self, epoch):
+        self.model.eval()
+
+        val_pbar = tqdm(
+            total=len(self.valloader),
+            ncols=0,
+            desc='                Val'
+        )
+
+        all_labels = []
+        all_preds = []
+        val_loss_meter = AccAverageMeter()
+        with torch.no_grad():
+            for i, (batch_imgs, batch_labels) in enumerate(self.valloader):
+                batch_imgs = batch_imgs.cuda()
+                batch_labels = batch_labels.cuda()
+                batch_probs = self.model(batch_imgs)
+                batch_preds = batch_probs.max(1)[1]
+                avg_loss = self.criterion(batch_probs, batch_labels)
+                val_loss_meter.update(avg_loss.item(), 1)
+
+                all_labels.extend(batch_labels.cpu().numpy().tolist())
+                all_preds.extend(batch_preds.cpu().numpy().tolist())
+
+                val_pbar.update()
+
+        val_mr = metrics.balanced_accuracy_score(all_labels, all_preds)
+        val_recalls = metrics.recall_score(all_labels, all_preds, average=None)
+        val_recalls = np.around(val_recalls, decimals=2).tolist()
+
+        val_pbar.set_postfix_str('Loss:{:.2f} MR:{:.2%}'.format(
+            val_loss_meter.avg, val_mr))
+        val_pbar.close()
+
+        return val_mr, val_recalls, val_loss_meter.avg
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--local_rank', type=int, help='Local Rank for\
+                        distributed training. if single-GPU, default: -1')
+    parser.add_argument('--config_path', type=str, help='path of config file')
+    args = parser.parse_args()
+    return args
+
+
+def _set_seed(seed=0):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+
+
+def main(args):
+    warnings.filterwarnings('ignore')
+    _set_seed()
+    # set_trace()
+    with open(args.config_path, 'r') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    trainer = Trainer(local_rank=args.local_rank, config=config)
+    trainer.train()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
