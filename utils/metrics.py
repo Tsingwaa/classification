e6703fa6ae448d35616deec9633ce0d68070e6c8
Tsingwaa <zengchh3@gmail.com>
2021-11-12 15:21:55 +0800

    update

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def euclidean_distance(qf, gf):
@@ -134,3 +135,82 @@ class R1_mAP():
         cmc, mAP = eval_func(distmat, q_pids, g_pids, q_camids, g_camids,
                              max_rank=self.max_rank)
         return cmc, mAP, distmat, self.pids, self.camids, gf
+
+
+class FusionMatrix(object):
+    def __init__(self, num_classes):
+        self.num_classes = num_classes
+        self.reset()
+
+    def reset(self):
+        self.matrix = np.zeros((self.num_classes, self.num_classes), dtype=int)
+
+    def update(self, output, label):
+        length = output.shape[0]
+        for i in range(length):
+            self.matrix[output[i], label[i]] += 1
+
+    def get_rec_per_class(self):
+        rec = np.array(
+            [
+                self.matrix[i, i] / self.matrix[:, i].sum()
+                for i in range(self.num_classes)
+            ]
+        )
+        rec[np.isnan(rec)] = 0
+        return rec
+
+    def get_pre_per_class(self):
+        pre = np.array(
+            [
+                self.matrix[i, i] / self.matrix[i, :].sum()
+                for i in range(self.num_classes)
+            ]
+        )
+        pre[np.isnan(pre)] = 0
+        return pre
+
+    def get_accuracy(self):
+        acc = (
+            np.sum([self.matrix[i, i] for i in range(self.num_classes)])
+            / self.matrix.sum()
+        )
+        return acc
+
+    def plot_confusion_matrix(self, normalize=False, cmap=plt.cm.Blues):
+
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+        # Compute confusion matrix
+        cm = self.matrix.T
+
+        fig, ax = plt.subplots()
+        im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+        ax.figure.colorbar(im, ax=ax)
+        # We want to show all ticks...
+        ax.set(xticks=np.arange(cm.shape[1]),
+               yticks=np.arange(cm.shape[0]),
+               # ... and label them with the respective list entries
+               xticklabels=np.arange(self.num_classes),
+               yticklabels=np.arange(self.num_classes),
+               title=title,
+               ylabel='True label',
+               xlabel='Predicted label')
+
+        # Rotate the tick labels and set their alignment.
+        plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+                 rotation_mode="anchor")
+
+        # Loop over data dimensions and create text annotations.
+        fmt = '.2f' if normalize else 'd'
+        thresh = cm.max() / 2.
+        for i in range(cm.shape[0]):
+            for j in range(cm.shape[1]):
+                ax.text(j, i, format(cm[i, j], fmt),
+                        ha="center", va="center",
+                        color="white" if cm[i, j] > thresh else "black")
+        fig.tight_layout()
+        return fig
