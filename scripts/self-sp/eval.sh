b4a63f32f06a823e72301a16d8165483d374d5ed
Tsingwaa.31 <zengchh3@gmail.com>
2021-11-14 22:24:27 +0800

    update self-sp

--- scripts/self-sp/eval.sh
+++ scripts/self-sp/eval.sh
@@ -12,4 +12,4 @@ export PYTHONPATH=$PYTHONPATH:$HOME/Projects/classification/
 # CUDA_VISIBLE_DEVICES=0 python3 eval.py --config_path 'configs/CIFAR10_0.01/20211014_CIFAR10_0.01_reverse_adapt_randaug_m15n2.yaml'
 
 CUDA_VISIBLE_DEVICES=0 python3 eval.py --local_rank -1 --config_path \
-        'configs/miniIN20_0.05/20211114_r18_2head_fc.yaml'
+        'configs/miniIN20_0.05/20211114_r18_2head_layer4_prog.yaml'
