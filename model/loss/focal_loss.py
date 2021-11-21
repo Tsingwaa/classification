a9b0c4e3fdff4712efbf5152a7860c67f0fe40f2
Tsingwaa.31 <zengchh3@gmail.com>
2021-11-21 20:08:46 +0800

    modify CE to add reweight module

--- model/loss/focal_loss.py
+++ model/loss/focal_loss.py
@@ -1,14 +1,13 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
 from model.loss.builder import Losses
 
 
 @Losses.register_module("FocalLoss")
 class FocalLoss(nn.modules.loss._WeightedLoss):
-    def __init__(self, weight=None, gamma=2,reduction='mean'):
-        super(FocalLoss, self).__init__(weight,reduction=reduction)
+    def __init__(self, weight=None, gamma=2, reduction='mean'):
+        super(FocalLoss, self).__init__(weight, reduction=reduction)
         self.gamma = gamma
         self.weight = weight
         # weight parameter will act as the alpha parameter
@@ -16,9 +15,10 @@ class FocalLoss(nn.modules.loss._WeightedLoss):
 
     def forward(self, input, target):
 
-        ce_loss = F.cross_entropy(input, target,
+        CE_loss = F.cross_entropy(input, target,
                                   reduction=self.reduction,
                                   weight=self.weight)
-        pt = torch.exp(-ce_loss)
-        focal_loss = ((1 - pt) ** self.gamma * ce_loss).mean()
+        pt = torch.exp(-CE_loss)
+        focal_loss = ((1 - pt) ** self.gamma * CE_loss).mean()
+
         return focal_loss
--- model/loss/softmax_loss.py
+++ model/loss/softmax_loss.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from model.loss.builder import Losses
 
 
@@ -40,15 +41,45 @@ class CrossEntropyLabelSmooth(nn.Module):
 
 
 @Losses.register_module("CrossEntropyLoss")
-class CrossEntropyLoss(nn.Module):
-    def __init__(self, **kwargs):
-        super(CrossEntropyLoss, self).__init__()
-        self.loss = nn.CrossEntropyLoss()
+class CELoss(nn.Module):
+    """This criterion computes the cross entropy loss between input and target.
 
-    def forward(self, X, y):
-        y = y.cuda()
-        loss = self.loss(X, y)
-        return loss
+    It is useful when training a classification problem with `C` classes.
+    If provided, the optional argument :attr:`weight` should be a 1D `Tensor`
+    assigning weight to each of the classes.
+    This is particularly useful when you have an imbalanced training set.
+    Args:
+        weight (Tensor, optional): a manual rescaling weight given to each
+            class. If given, has to be a Tensor of size `C`
+        reduction (string, optional): Specifies the reduction to apply to
+            the output: ``'none'`` | ``'mean'`` | ``'sum'``.
+            ``'none'``: no reduction will be applied
+            ``'mean'``: the weighted mean of the output is taken,
+            ``'sum'``: the output will be summed.
+            Default: ``'mean'``
+        label_smoothing (float, optional): A float in [0.0, 1.0]. Specifies the
+            amount of smoothing when computing the loss, where 0.0 means no
+            smoothing. The targets become a mixture of the original ground
+            truth and a uniform distribution as described in `Rethinking the
+            Inception Architecture for Computer Vision
+            <https://arxiv.org/abs/1512.00567>`.
+            Default: :math:`0.0`.
+    """
+
+    def __init__(self, weight=None, reduction='mean', label_smoothing=0.,
+                 **kwargs):
+        super(CELoss, self).__init__()
+        self.weight = weight
+        self.reduction = reduction
+        self.label_smoothing = label_smoothing
+
+    def forward(self, input_, target):
+        if self.weight is not None:
+            self.weight = self.weight.cuda()
+        return F.cross_entropy(input_, target,
+                               weight=self.weight,
+                               reduction=self.reduction,
+                               label_smoothing=self.label_smoothing)
 
 
 @Losses.register_module("BCEwithLogitsLoss")
@@ -58,6 +89,5 @@ class BCEWithLogitsLoss(nn.Module):
         self.loss = nn.BCEWithLogitsLoss()
 
     def forward(self, X, y):
-        y = y.cuda()
         loss = self.loss(X, y)
         return loss
