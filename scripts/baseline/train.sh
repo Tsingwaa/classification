2bcefcd71a59dee53b9bde7be26fbf4d0fb76a25
呈華 <zengchh3@gmail.com>
2021-11-25 15:32:47 +0800

    modify 3step dataset to few-medium-many

--- scripts/baseline/train.sh
+++ scripts/baseline/train.sh
@@ -6,10 +6,10 @@ export PYTHONPATH=$PYTHONPATH:$HOME/Projects/classification
 
 # Single-GPU Training
 # CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1\
-#         --config_path 'configs/miniIN20_0.05/20211123_r18.yaml'
-# CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1\
 #         --config_path 'configs/miniIN20_0.05/20211123_r18_rw.yaml'
 CUDA_VISIBLE_DEVICES=1 python3 train.py --local_rank -1\
-        --config_path 'configs/miniIN20_0.05_4step/20211123_r18.yaml'
+        --config_path 'configs/miniIN20_0.05_3step/20211125_r18.yaml'
 CUDA_VISIBLE_DEVICES=1 python3 train.py --local_rank -1\
-        --config_path 'configs/miniIN20_0.05_4step/20211123_r18_rw.yaml'
+        --config_path 'configs/miniIN20_0.05_3step/20211125_r18_rw.yaml'
+CUDA_VISIBLE_DEVICES=1 python3 train.py --local_rank -1\
+        --config_path 'configs/miniIN20_0.05_3step/20211125_r18_focal.yaml'
