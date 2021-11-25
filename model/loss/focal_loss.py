a852dbf9efc28b084ba2ff7728176644249381ab
Tsingwaa.31 <zengchh3@gmail.com>
2021-11-25 17:59:44 +0800

    Modify Focal Loss

--- model/loss/focal_loss.py
+++ model/loss/focal_loss.py
@@ -6,13 +6,16 @@ from model.loss.builder import Losses
 
 @Losses.register_module("FocalLoss")
 class FocalLoss(nn.Module):
-    def __init__(self, alpha=0.25, gamma=2, **kwargs):
+    def __init__(self, weight=None, gamma=2, **kwargs):
         super(FocalLoss, self).__init__()
-        self.alpha = alpha
+        self.weight = weight  # 1*C
         self.gamma = gamma
 
     def forward(self, inputs, targets):
-        ce_loss = F.cross_entropy(inputs, targets, reduction='none')
-        pt = torch.exp(-ce_loss)
-        focal_loss = (self.alpha * (1 - pt) ** self.gamma * ce_loss).mean()
+        ce_loss = F.cross_entropy(inputs, targets, reduction='none')  # N*1
+        pt = torch.exp(-ce_loss)  # N*1: softmax prob of the target class
+        if self.weight is not None:
+            self.weight = self.weight.cuda()
+            ce_loss *= self.weight[targets]  # (N*1) * (N*1)
+        focal_loss = ((1 - pt) ** self.gamma * ce_loss).mean()  # 1*1
         return focal_loss
