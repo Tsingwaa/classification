a7c5d5bf7ea36408232600a4e9d823e93c2d43e9
Tsingwaa.31 <zengchh3@gmail.com>
2021-11-28 00:05:21 +0800

    add last 20 epochs average mean recall

--- scripts/adv/train.sh
+++ scripts/adv/train.sh
@@ -6,9 +6,15 @@ export PYTHONPATH=$PYTHONPATH:$HOME/Projects/classification
 #     train.py --config_path "configs/miniImageNet/20211024_resnet18.yaml"
 
 # Single-GPU Training
-CUDA_VISIBLE_DEVICES=1 python3 train.py --local_rank -1 --config_path \
-        'configs/miniIN20_0.05/20211126_r18_LinfPGD_joint0.8_dualBN.yaml'
-CUDA_VISIBLE_DEVICES=1 python3 train.py --local_rank -1 --config_path \
-        'configs/miniIN20_0.05/20211126_r18_LinfPGD_joint0.8_dualBN_adapt2.yaml'
-CUDA_VISIBLE_DEVICES=1 python3 train.py --local_rank -1 --config_path \
-        'configs/miniIN20_0.05/20211126_r18_LinfPGD_joint0.8_dualBN_adapt.yaml'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1 --config_path \
+        'configs/miniIN20_0.05_3step/20211125_r18_LinfPGD_joint0.5.yaml'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1 --config_path \
+        'configs/miniIN20_0.05_3step/20211125_r18_LinfPGD_joint0.5_adapt2.yaml'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1 --config_path \
+        'configs/miniIN20_0.05_3step/20211125_r18_LinfPGD_joint0.5_adapt.yaml'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1 --config_path \
+        'configs/miniIN20_0.05_3step/20211126_r18_LinfPGD_joint0.8.yaml'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1 --config_path \
+        'configs/miniIN20_0.05_3step/20211126_r18_LinfPGD_joint0.8_adapt2.yaml'
+CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1 --config_path \
+        'configs/miniIN20_0.05_3step/20211126_r18_LinfPGD_joint0.8_adapt.yaml'
