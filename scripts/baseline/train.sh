6033912f9f7507d20765ef5c2d1390779b9d12c3
Tsingwaa.31 <zengchh3@gmail.com>
2021-11-28 16:17:10 +0800

    add resample script for 3_step dataset

--- scripts/baseline/train.sh
+++ scripts/baseline/train.sh
@@ -6,16 +6,16 @@ export PYTHONPATH=$PYTHONPATH:$HOME/Projects/classification
 
 # Single-GPU Training
 CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1\
-        --config_path 'configs/miniIN20_0.05/20211127_r18_rw.yaml'
+        --config_path 'configs/miniIN20_0.05_3step/20211125_r18_balsample.yaml'
 CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1\
-        --config_path 'configs/miniIN20_0.05/20211127_r18_focal.yaml'
-CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1\
-        --config_path 'configs/miniIN20_0.05/20211127_r18_rw_focal.yaml'
-CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1\
-        --config_path 'configs/miniIN20_0.05/20211127_r18.yaml'
-CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1\
-        --config_path 'configs/miniIN20_0.05/20211127_r34.yaml'
-CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1\
-        --config_path 'configs/miniIN20_0.05/20211127_r18_balsample.yaml'
-CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1\
-        --config_path 'configs/miniIN20_0.05/20211127_r18_oversample.yaml'
+        --config_path 'configs/miniIN20_0.05_3step/20211125_r18_oversample.yaml'
+# CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1\
+#         --config_path 'configs/miniIN20_0.05/20211127_r18_rw_focal.yaml'
+# CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1\
+#         --config_path 'configs/miniIN20_0.05/20211127_r18.yaml'
+# CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1\
+#         --config_path 'configs/miniIN20_0.05/20211127_r34.yaml'
+# CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1\
+#         --config_path 'configs/miniIN20_0.05/20211127_r18_balsample.yaml'
+# CUDA_VISIBLE_DEVICES=0 python3 train.py --local_rank -1\
+#         --config_path 'configs/miniIN20_0.05/20211127_r18_oversample.yaml'
