9726a3303c238e1a4f32ea60a782cb7cb9aadc80
Tsingwaa.Lab <zengchh3@gmail.com>
2021-12-10 03:14:27 +0800

    update

--- scripts/baseline/train.sh
+++ scripts/baseline/train.sh
@@ -7,16 +7,16 @@ export PYTHONPATH=$PYTHONPATH:$HOME/Projects/classification
 
 # Single-GPU Training
 # Noise
-# CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
-#         --config_path 'configs/miniIN20_0.05/r18_noise0.1.yaml'
-# CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
-#         --config_path 'configs/miniIN20_0.05/r18_noise0.05.yaml'
-# CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
-#         --config_path 'configs/miniIN20_0.05/r18_noise0.01.yaml'
-# CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
-#         --config_path 'configs/miniIN20_0.05/r18_noise0.005.yaml'
-# CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
-#         --config_path 'configs/miniIN20_0.05/r18_noise0.001.yaml'
+CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
+        --config_path 'configs/miniIN20_0.05/r18_noise0.1.yaml'
+CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
+        --config_path 'configs/miniIN20_0.05/r18_noise0.05.yaml'
+CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
+        --config_path 'configs/miniIN20_0.05/r18_noise0.01.yaml'
+CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
+        --config_path 'configs/miniIN20_0.05/r18_noise0.005.yaml'
+CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
+        --config_path 'configs/miniIN20_0.05/r18_noise0.001.yaml'
 # CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
 #         --config_path 'configs/miniIN20_0.05/r18_noise0.01_OS.yaml'
 # CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
@@ -27,24 +27,24 @@ export PYTHONPATH=$PYTHONPATH:$HOME/Projects/classification
 #         --config_path 'configs/miniIN20_0.05/r18_noise0.001-0.01.yaml'
 
 # Baseline
-CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
-        --config_path 'configs/miniIN20_0.05/r18.yaml'
-CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
-        --config_path 'configs/miniIN20_0.05/r18_OS.yaml'
-CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
-        --config_path 'configs/miniIN20_0.05/r18_RS.yaml'
-CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
-        --config_path 'configs/miniIN20_0.05/r18_RW.yaml'
-CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
-        --config_path 'configs/miniIN20_0.05/r34.yaml'
-CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
-        --config_path 'configs/miniIN20_0.05/r18_CB0.99.yaml'
-CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
-        --config_path 'configs/miniIN20_0.05/r18_CB0.99_focal1.yaml'
-CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
-        --config_path 'configs/miniIN20_0.05/r18_focal1.yaml'
-CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
-        --config_path 'configs/miniIN20_0.05/r18_focal2.yaml'
+# CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
+#         --config_path 'configs/miniIN20_0.05/r18.yaml'
+# CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
+#         --config_path 'configs/miniIN20_0.05/r18_OS.yaml'
+# CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
+#         --config_path 'configs/miniIN20_0.05/r18_RS.yaml'
+# CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
+#         --config_path 'configs/miniIN20_0.05/r18_RW.yaml'
+# CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
+#         --config_path 'configs/miniIN20_0.05/r34.yaml'
+# CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
+#         --config_path 'configs/miniIN20_0.05/r18_CB0.99.yaml'
+# CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
+#         --config_path 'configs/miniIN20_0.05/r18_CB0.99_focal1.yaml'
+# CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
+#         --config_path 'configs/miniIN20_0.05/r18_focal1.yaml'
+# CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
+#         --config_path 'configs/miniIN20_0.05/r18_focal2.yaml'
 
 # Augments
 # CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1\
