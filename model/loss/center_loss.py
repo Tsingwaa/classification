ff419c20a83546aaba142767d25f5043ab5f178f
Tsingwaa.31 <zengchh3@gmail.com>
2021-12-15 14:36:51 +0800

    modify center loss

--- model/loss/center_loss.py
+++ model/loss/center_loss.py
@@ -7,18 +7,15 @@ from model.loss.builder import Losses
 
 @Losses.register_module("CenterLoss")
 class CenterLoss(nn.Module):
-    """Center loss.
-
-    Reference:
-    Wen et al. A Discriminative Feature Learning Approach for Deep\
-            Face Recognition. ECCV 2016.
+    """Reference: Wen et al. A Discriminative Feature Learning Approach for
+    Deep Face Recognition. ECCV 2016.
 
     Args:
         num_classes (int): number of classes.
         feat_dim (int): feature dimension.
     """
 
-    def __init__(self, num_classes=751, feat_dim=2048, use_gpu=True, **kwargs):
+    def __init__(self, num_classes=10, feat_dim=2, use_gpu=True, **kwargs):
         super(CenterLoss, self).__init__()
         self.num_classes = num_classes
         self.feat_dim = feat_dim
@@ -36,8 +33,7 @@ class CenterLoss(nn.Module):
             x: feature matrix with shape (batch_size, feat_dim).
             labels: ground truth labels with shape (num_classes).
         """
-        assert x.size(0) == labels.size(0),\
-            "features.size(0) is not equal to labels.size(0)"
+        assert x.size(0) == labels.size(0), "features size != labels size"
 
         batch_size = x.size(0)
         distmat = torch.pow(x, 2).sum(dim=1, keepdim=True)\
@@ -45,35 +41,41 @@ class CenterLoss(nn.Module):
             torch.pow(self.centers, 2).sum(dim=1, keepdim=True)\
             .expand(self.num_classes, batch_size).t()
 
-        distmat.addmm_(1, -2, x, self.centers.t())
+        distmat.addmm_(x, self.centers.t(), beta=1, alpha=-2)
 
         classes = torch.arange(self.num_classes).long()
         if self.use_gpu:
             classes = classes.cuda()
+
         labels = labels.unsqueeze(1).expand(batch_size, self.num_classes)
         mask = labels.eq(classes.expand(batch_size, self.num_classes))
 
-        dist = []
-        for i in range(batch_size):
-            value = distmat[i][mask[i]]
-            # for numerical stability
-            value = value.clamp(min=1e-12, max=1e+12)
-            dist.append(value)
-        dist = torch.cat(dist)
-        loss = dist.mean()
+        # dist = []
+        # for i in range(batch_size):
+        #     value = distmat[i][mask[i]]
+        #     # for numerical stability
+        #     value = value.clamp(min=1e-12, max=1e+12)
+        #     dist.append(value)
+        # dist = torch.cat(dist)
+        # loss = dist.mean()
+        dist = distmat * mask.float()
+        loss = dist.clamp(min=1e-12, max=1e+12).sum() / batch_size
+
         return loss
 
 
 if __name__ == '__main__':
     use_gpu = False
-    center_loss = CenterLoss(use_gpu=use_gpu)
-    features = torch.rand(16, 2048)
-    targets = torch.Tensor(
-        [0, 1, 2, 3, 2, 3, 1, 4, 5, 3, 2, 1, 0, 0, 5, 4]).long()
+    num_classes = 10
+    feat_dim = 2
+
+    features = torch.ones(16, 2)
+    targets = torch.ones((16,)).long()
     if use_gpu:
-        features = torch.rand(16, 2048).cuda()
-        targets = torch.Tensor(
-            [0, 1, 2, 3, 2, 3, 1, 4, 5, 3, 2, 1, 0, 0, 5, 4]).cuda()
+        features = features.cuda()
+        targets = targets.cuda()
+
+    center_loss = CenterLoss(num_classes, feat_dim, use_gpu)
 
     loss = center_loss(features, targets)
     print(loss)
