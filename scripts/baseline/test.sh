149cdc19ee7cd35facd53faec7144ec1f0ef7b6c
waa <zengchh3@gmail.com>
2022-01-14 14:53:25 +0800

    update

---
+++ scripts/baseline/test.sh
@@ -0,0 +1,7 @@
+export PYTHONPATH=$PYTHONPATH:$HOME/Projects/classification
+
+# Distributed Testing
+
+CUDA_VISIBLE_DEVICES=2,4 python3 -W ignore -m torch.distributed.launch\
+    --nproc_per_node=2 --master_addr 127.0.0.111 --master_port 30003 \
+    test.py --config_path "configs/ImageNet_LT/rx50_CE.yaml"
--- scripts/baseline/train.sh
+++ scripts/baseline/train.sh
@@ -1,8 +1,8 @@
 export PYTHONPATH=$PYTHONPATH:$HOME/Projects/classification
 
 # Distributed Training
-CUDA_VISIBLE_DEVICES=2,4 python3 -W ignore -m torch.distributed.launch\
-    --nproc_per_node=2 --master_addr 127.0.0.111 --master_port 30000 \
+CUDA_VISIBLE_DEVICES=0,1 python3 -W ignore -m torch.distributed.launch\
+    --nproc_per_node=2 --master_addr 127.0.0.111 --master_port 30006 \
     train.py --config_path "configs/ImageNet_LT/rx50_CE.yaml"
 
 # Baseline
