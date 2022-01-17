6af601e88eca077079c973560266a0d06aab73e2
waa <zengchh3@gmail.com>
2022-01-17 09:16:52 +0800

    update

--- scripts/cutmix/train.sh
+++ scripts/cutmix/train.sh
@@ -1,10 +1,13 @@
 export PYTHONPATH=$PYTHONPATH:$HOME/Projects/classification
 
 # Distributed Training
-CUDA_VISIBLE_DEVICES=0,1 python3 -W ignore -m torch.distributed.launch\
-    --nproc_per_node=2 --master_addr 127.0.0.111 --master_port 30001 \
-    train.py --config_path "configs/ImageNet_LT/rx50_adapt2_2_0.5.yaml"
+CUDA_VISIBLE_DEVICES=4,5,6,7 python3 -W ignore -m torch.distributed.launch\
+    --nproc_per_node=4 --master_addr 127.0.0.111 --master_port 30002 \
+    train.py --config_path "configs/ImageNet_LT/rx50_adapt2_remix_v2_4_0.6.yaml"
 
+CUDA_VISIBLE_DEVICES=4,5,6,7 python3 -W ignore -m torch.distributed.launch\
+    --nproc_per_node=4 --master_addr 127.0.0.111 --master_port 30002 \
+    train.py --config_path "configs/ImageNet_LT/rx50_adapt2_remix_v2_4_0.5.yaml"
 # Baseline
 # CUDA_VISIBLE_DEVICES=$1 python3 train.py --local_rank -1 --config_path $2
 
