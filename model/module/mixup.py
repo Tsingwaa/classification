c793bec351769a7683cd8334543130d91223917e
waa <zengchh3@gmail.com>
2022-01-18 17:05:16 +0800

    update

--- model/module/mixup.py
+++ model/module/mixup.py
@@ -1,5 +1,6 @@
-import numpy as np
 import random
+
+import numpy as np
 import torch
 from torch.utils.data.dataset import Dataset
 
@@ -7,7 +8,13 @@ from torch.utils.data.dataset import Dataset
 class MixUp(Dataset):
     """Referred to https://github.com/ildoonet/cutmix"""
 
-    def __init__(self, dataset, num_mix=1, beta=1., prob=1.0, type='random', remix=False):
+    def __init__(self,
+                 dataset,
+                 num_mix=1,
+                 beta=1.,
+                 prob=1.0,
+                 mode='random',
+                 remix=False):
         """build cutmix dataset based on common custom dataset class.
 
         Args:
@@ -22,15 +29,17 @@ class MixUp(Dataset):
         self.num_mix = num_mix
         self.beta = beta
         self.prob = prob
-        self.type = type
+        self.mode = mode
         self.remix = remix
         self.idx_per_class = self.get_idx_per_class()
 
     def __getitem__(self, index):
         img, lb = self.dataset[index]
         lb_onehot = single_label2onehot(self.num_classes, lb)
+
         for _ in range(self.num_mix):
             r = np.random.rand(1)
+
             if self.beta <= 0 or r > self.prob:
                 continue
             lambd = np.random.beta(self.beta, self.beta)
@@ -52,74 +61,73 @@ class MixUp(Dataset):
 
         return img, lb_onehot
 
-    def get_img2(self, lb):
-        if self.type == 'random':
-            return self.get_img2_random(lb)
-        elif self.type == 'A':
-            return self.get_img2_A(lb)
-        elif self.type == 'B':
-            return self.get_img2_B(lb)
-        elif self.type == 'C':
-            return self.get_img2_C(lb)
-        elif self.type == 'D':
-            return self.get_img2_D(lb)
-        elif self.type == 'E':
-            return self.get_img2_E(lb)
+    def get_img2(self, label):
+        if self.mode == 'random':
+            return self.get_img2_random(label)
+        elif self.mode == 'A':
+            return self.get_img2_A(label)
+        elif self.mode == 'B':
+            return self.get_img2_B(label)
+        elif self.mode == 'C':
+            return self.get_img2_C(label)
+        elif self.mode == 'D':
+            return self.get_img2_D(label)
+        elif self.mode == 'E':
+            return self.get_img2_E(label)
         else:
-            return None, None
+            raise ValueError
 
     def get_img2_A(self, lb):
-        # generate mixed sample, lambd for original area
         rand_index = random.choice(range(len(self)))
         img2, lb2 = self.dataset[rand_index]
+
         while lb2 > lb:
             # print('lb: %d, lb2: %d'%(lb, lb2))
             rand_index = random.choice(range(len(self)))
             img2, lb2 = self.dataset[rand_index]
+
         return img2, lb2
 
     def get_img2_B(self, lb):
-        # generate mixed sample, lambd for original area
         rand_index = random.choice(range(len(self)))
         img2, lb2 = self.dataset[rand_index]
+
         while lb2 < lb:
             # print('lb: %d, lb2: %d'%(lb, lb2))
             rand_index = random.choice(range(len(self)))
             img2, lb2 = self.dataset[rand_index]
+
         return img2, lb2
 
     def get_img2_C(self, lb):
-        # weight = np.array(self.num_samples_per_cls) / sum(self.num_samples_per_cls)
-        # weight = np.exp(weight) / sum(np.exp(weight))
-
         weight = np.power(self.num_samples_per_cls, 2)
-        weight = weight / sum(weight)
-        # generate mixed sample, lambd for original area
         class_idx = np.random.choice(range(self.num_classes), p=weight)
         rand_index = np.random.choice(self.idx_per_class[class_idx])
         img2, lb2 = self.dataset[rand_index]
+
         return img2, lb2
-    
+
     def get_img2_D(self, lb):
-        # generate mixed sample, lambd for original area
         rand_index = random.choice(range(len(self)))
         img2, lb2 = self.dataset[rand_index]
+
         while abs(lb - lb2) > np.log(self.num_classes):
             # print('lb: %d, lb2: %d'%(lb, lb2))
             rand_index = random.choice(range(len(self)))
             img2, lb2 = self.dataset[rand_index]
+
         return img2, lb2
-    
+
     def get_img2_E(self, lb):
-        # generate mixed sample, lambd for original area
         rand_index = random.choice(range(len(self.idx_per_class[lb])))
         img2, lb2 = self.dataset[rand_index]
+
         return img2, lb2
-    
+
     def get_img2_random(self, lb):
-        # generate mixed sample, lambd for original area
         rand_index = random.choice(range(len(self)))
         img2, lb2 = self.dataset[rand_index]
+
         return img2, lb2
 
     def __len__(self):
@@ -127,15 +135,17 @@ class MixUp(Dataset):
 
     def get_idx_per_class(self):
         idx_per_class = [[] for i in range(self.num_classes)]
-        
+
         for i in range(len(self.dataset)):
             _, lb = self.dataset[i]
             idx_per_class[lb].append(i)
-        
+
         return idx_per_class
+
 
 def single_label2onehot(size, target):
     # single label to one-hot
     vec = torch.zeros(size, dtype=torch.float32)
     vec[target] = 1.
+
     return vec
