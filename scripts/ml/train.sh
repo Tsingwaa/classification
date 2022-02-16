37d28613799ed37800bce6819152d0d49ddd44ae
waa <zengchh3@gmail.com>
2022-02-16 09:46:01 +0800

    update

--- scripts/ml/train.sh
+++
@@ -1,8 +0,0 @@
-export PYTHONPATH=$HOME/Projects/classification
-
-# Distributed Training without amp
-# OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$1 torchrun --nproc_per_node=$2 \
-#     --master_addr 127.0.0.1 --master_port 30000 train.py --config_path $3
-
-# Single GPU
-CUDA_VISIBLE_DEVICES=$2 python3 train.py --config_path $1
--- scripts/ml/train_ce.py
+++ scripts/ml/train_ce.py
@@ -1,5 +1,6 @@
 """trainer script """
 import argparse
+import logging
 import os
 import random
 import warnings
@@ -10,13 +11,12 @@ import torch
 import yaml
 from base.base_trainer import BaseTrainer
 from prefetch_generator import BackgroundGenerator
-# from pudb import set_trace
 from torch import distributed as dist
 from torch.nn import SyncBatchNorm
 from torch.nn.parallel import DistributedDataParallel
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from utils import AverageMeter
+from utils import AverageMeter, ExpStat
 
 
 class DataLoaderX(DataLoader):
@@ -94,9 +94,7 @@ class Trainer(BaseTrainer):
         if self.local_rank != -1:
             self.model = SyncBatchNorm.convert_sync_batchnorm(self.model)
             self.model = DistributedDataParallel(self.model,
-                                                 device_ids=[self.local_rank],
-                                                 output_device=self.local_rank,
-                                                 find_unused_parameters=True)
+                                                 device_ids=[self.local_rank])
 
         #######################################################################
         # Initialize Loss
@@ -126,6 +124,14 @@ class Trainer(BaseTrainer):
         #######################################################################
 
         if self.local_rank in [-1, 0]:
+            best_mr = 0.0
+            best_epoch = 1
+            best_group_mr = []
+            # average of mean recall in the last several epochs(default: 5)
+            last_mrs = []  # General: include all classes.
+            last_maj_mrs = []  # Majority classes: > 100 images
+            last_med_mrs = []  # Medium classes: 20 ~ 100 images
+            last_min_mrs = []  # Minority classes: < 20 images
             start_time = datetime.now()
 
         self.final_epoch = self.start_epoch + self.total_epochs
@@ -142,7 +148,7 @@ class Trainer(BaseTrainer):
                 train_sampler.set_epoch(cur_epoch)
                 val_sampler.set_epoch(cur_epoch)
 
-            train_loss = self.train_epoch(
+            train_stat, train_loss = self.train_epoch(
                 cur_epoch=cur_epoch,
                 trainloader=self.trainloader,
                 model=self.model,
@@ -151,7 +157,7 @@ class Trainer(BaseTrainer):
                 dataset=trainset,
             )
 
-            val_loss = self.evaluate(
+            val_stat, val_loss = self.evaluate(
                 cur_epoch=cur_epoch,
                 valloader=self.valloader,
                 model=self.model,
@@ -160,30 +166,111 @@ class Trainer(BaseTrainer):
             )
 
             if self.local_rank in [-1, 0]:
+                if self.final_epoch - cur_epoch <= 5:
+                    last_mrs.append(val_stat.mr)
+                    last_maj_mrs.append(val_stat.group_mr[0])
+                    last_med_mrs.append(val_stat.group_mr[1])
+                    last_min_mrs.append(val_stat.group_mr[2])
+
                 # log message into file "train.log" in the self.save_dir
                 self.log(
                     f"Epoch[{cur_epoch:>3d}/{self.final_epoch-1}] "
                     f"LR:{self.optimizer.param_groups[0]['lr']:.1e} "
                     f"Trainset Loss={train_loss:>4.1f} "
-                    " || "
-                    f"Valset Loss={val_loss:>4.1f} ",
-                    log_level='file')
+                    f"MR={train_stat.mr:>7.2%}"
+                    f"[{train_stat.group_mr[0]:>7.2%}, "
+                    f"{train_stat.group_mr[1]:>7.2%}, "
+                    f"{train_stat.group_mr[2]:>7.2%}]"
+                    f" || "
+                    f"Valset Loss={val_loss:>4.1f} "
+                    f"MR={val_stat.mr:>6.2%}"
+                    f"[{val_stat.group_mr[0]:>6.2%}, "
+                    f"{val_stat.group_mr[1]:>6.2%}, "
+                    f"{val_stat.group_mr[2]:>6.2%}]",
+                    log_level="file")
 
-                if not cur_epoch % self.save_period:
-                    self.save_checkpoint(epoch=cur_epoch,
-                                         model=self.model,
-                                         optimizer=self.optimizer,
-                                         is_best=False,
-                                         stat=None,
-                                         prefix=f"seed{self.seed}",
-                                         save_dir=self.exp_dir)
+                # Save log by tensorboard
+                self.writer.add_scalar(
+                    f"{self.exp_name}/LR",
+                    self.optimizer.param_groups[-1]["lr"],
+                    cur_epoch,
+                )
+                self.writer.add_scalars(
+                    f"{self.exp_name}/Loss",
+                    {
+                        "train_loss": train_loss,
+                        "val_loss": val_loss
+                    },
+                    cur_epoch,
+                )
+                self.writer.add_scalars(
+                    f"{self.exp_name}/Recall",
+                    {
+                        "train_mr": train_stat.mr,
+                        "val_mr": val_stat.mr
+                    },
+                    cur_epoch,
+                )
+                self.writer.add_scalars(
+                    f"{self.exp_name}/TrainGroupRecall",
+                    {
+                        "maj_mr": train_stat.group_mr[0],
+                        "med_mr": train_stat.group_mr[1],
+                        "min_mr": train_stat.group_mr[2],
+                    },
+                    cur_epoch,
+                )
+                self.writer.add_scalars(
+                    f"{self.exp_name}/ValGroupRecall",
+                    {
+                        "maj_mr": val_stat.group_mr[0],
+                        "med_mr": val_stat.group_mr[1],
+                        "min_mr": val_stat.group_mr[2],
+                    },
+                    cur_epoch,
+                )
+
+                is_best = val_stat.mr > best_mr
+
+                if is_best:
+                    best_mr = val_stat.mr
+                    best_epoch = cur_epoch
+                    best_group_mr = val_stat.group_mr
+
+                if (not cur_epoch % self.save_period) or is_best:
+                    self.save_checkpoint(
+                        epoch=cur_epoch,
+                        model=self.model,
+                        optimizer=self.optimizer,
+                        is_best=is_best,
+                        stat=val_stat,
+                        prefix=f"seed{self.seed}",
+                        save_dir=self.exp_dir,
+                    )
 
         if self.local_rank in [-1, 0]:
             end_time = datetime.now()
             dur_time = str(end_time - start_time)[:-7]  # 取到秒
 
+            final_mr = np.around(np.mean(last_mrs), decimals=4)
+            final_maj_mr = np.around(np.mean(last_maj_mrs), decimals=4)
+            final_med_mr = np.around(np.mean(last_med_mrs), decimals=4)
+            final_min_mr = np.around(np.mean(last_min_mrs), decimals=4)
+
             self.log(
-                f"\n===> Total Runtime: {dur_time}\n"
+                f"\n===> Total Runtime: {dur_time}\n\n"
+                f"===> Best mean recall:  (epoch{best_epoch}) {best_mr:>6.2%} "
+                f"[{best_group_mr[0]:>6.2%}, "
+                f"{best_group_mr[1]:>6.2%}, "
+                f"{best_group_mr[2]:>6.2%}]\n\n"
+                f"===> Last mean recall: {val_stat.mr:>6.2%} "
+                f"[{val_stat.group_mr[0]:>6.2%}, "
+                f"{val_stat.group_mr[1]:>6.2%}, "
+                f"{val_stat.group_mr[2]:>6.2%}]\n\n"
+                f"===> Final average mean recall of last 5 epochs: {final_mr:>6.2%} "
+                f"[{final_maj_mr:>6.2%}, "
+                f"{final_med_mr:>6.2%}, "
+                f"{final_min_mr:>6.2%}]\n\n"
                 f"===> Save directory: '{self.exp_dir}'\n"
                 f"*********************************************************"
                 f"*********************************************************\n")
@@ -195,16 +282,18 @@ class Trainer(BaseTrainer):
         if self.local_rank in [-1, 0]:
             train_pbar = tqdm(
                 total=len(trainloader),
-                ncols=120,
-                desc=f"Train Epoch[{cur_epoch:>3d}/{self.final_epoch-1}]")
+                ncols=132,
+                desc=f"Train Epoch[{cur_epoch:>3d}/{self.final_epoch-1}]",
+            )
 
         train_loss_meter = AverageMeter()
+        train_stat = ExpStat(dataset)
 
         for i, (batch_imgs, batch_targets) in enumerate(trainloader):
             batch_imgs = batch_imgs.cuda(non_blocking=True)
             batch_targets = batch_targets.cuda(non_blocking=True)
-            batch_embeddings = model(batch_imgs, out_type='vec')
-            avg_loss = criterion(batch_embeddings, batch_targets)
+            batch_probs = model(batch_imgs, out_type="fc")
+            avg_loss = criterion(batch_probs, batch_targets)
 
             optimizer.zero_grad()
             avg_loss.backward()
@@ -214,7 +303,9 @@ class Trainer(BaseTrainer):
                 dist.barrier()
                 avg_loss = self._reduce_tensor(avg_loss)
 
+            batch_preds = torch.argmax(batch_probs, dim=1)
             train_loss_meter.update(avg_loss.item(), 1)
+            train_stat.update(batch_targets, batch_preds)
 
             if self.local_rank in [-1, 0]:
                 train_pbar.update()
@@ -222,13 +313,23 @@ class Trainer(BaseTrainer):
                     f"LR:{optimizer.param_groups[0]['lr']:.1e} "
                     f"Loss:{train_loss_meter.avg:>3.1f}")
 
+        if self.local_rank != -1:
+            dist.barrier()
+            # 累加所有进程的train_stat记录的confusion matrix
+            train_stat._cm = self._reduce_tensor(train_stat._cm, op="sum")
+
         if self.local_rank in [-1, 0]:
             train_pbar.set_postfix_str(
                 f"LR:{optimizer.param_groups[0]['lr']:.1e} "
-                f"Loss:{train_loss_meter.avg:>4.2f}")
+                f"Loss:{train_loss_meter.avg:>4.2f} "
+                f"MR:{train_stat.mr:>7.2%} "
+                f"[{train_stat.group_mr[0]:>4.0%}, "
+                f"{train_stat.group_mr[1]:>4.0%}, "
+                f"{train_stat.group_mr[2]:>4.0%}]")
+
             train_pbar.close()
 
-        return train_loss_meter.avg
+        return train_stat, train_loss_meter.avg
 
     def evaluate(self, cur_epoch, valloader, model, criterion, dataset,
                  **kwargs):
@@ -240,44 +341,57 @@ class Trainer(BaseTrainer):
                             ncols=0,
                             desc=f"                 {desc}")
         val_loss_meter = AverageMeter()
+        val_stat = ExpStat(dataset)
 
         with torch.no_grad():
             for i, (batch_imgs, batch_targets) in enumerate(valloader):
                 batch_imgs = batch_imgs.cuda(non_blocking=True)
                 batch_targets = batch_targets.cuda(non_blocking=True)
-                batch_embeddings = model(batch_imgs, out_type='vec')
-                avg_loss = criterion(batch_embeddings, batch_targets)
+                batch_probs = model(batch_imgs, out_type="fc_128")
+                batch_preds = torch.argmax(batch_probs, dim=1)
+                avg_loss = criterion(batch_probs, batch_targets)
 
                 if self.local_rank != -1:
                     dist.barrier()
                     avg_loss = self._reduce_tensor(avg_loss)
 
                 val_loss_meter.update(avg_loss.item(), 1)
+                val_stat.update(batch_targets, batch_preds)
 
                 if self.local_rank in [-1, 0]:
                     val_pbar.update()
                     val_pbar.set_postfix_str(
                         f"Loss:{val_loss_meter.avg:>3.1f}")
 
+        if self.local_rank != -1:
+            dist.barrier()
+            val_stat._cm = self._reduce_tensor(val_stat._cm, op="sum")
+
         if self.local_rank in [-1, 0]:
-            val_pbar.set_postfix_str(f"Loss:{val_loss_meter.avg:>4.2f}")
+            val_pbar.set_postfix_str(f"Loss:{val_loss_meter.avg:>4.2f} "
+                                     f"MR:{val_stat.mr:>6.2%} "
+                                     f"[{val_stat.group_mr[0]:>3.0%}, "
+                                     f"{val_stat.group_mr[1]:>3.0%}, "
+                                     f"{val_stat.group_mr[2]:>3.0%}]")
             val_pbar.close()
 
-        return val_loss_meter.avg
+        return val_stat, val_loss_meter.avg
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     # Local rank设定：单卡默认为-1，多卡不设定，ddp自动设定为0,1,...
-    parser.add_argument("--local_rank",
-                        type=int,
-                        default=-1,
-                        help="Local Rank for distributed training. "
-                        "if single-GPU, default set to -1")
+    parser.add_argument(
+        "--local_rank",
+        type=int,
+        default=-1,
+        help="Local Rank for distributed training. "
+        "if single-GPU, default set to -1",
+    )
     parser.add_argument("--config_path", type=str, help="path of config file")
     parser.add_argument("--seed", type=int, default=0)
-    # parser.add_argument("--lr", default=0.5, type=float, help="learning rate")
-    # parser.add_argument("--wd", default=1e-4, type=float, help="weight decay")
+    # parser.add_argument("--lr", default=0.1, type=float, help="learning rate")
+    # parser.add_argument("--wd", default=5e-4, type=float, help="weight decay")
     args = parser.parse_args()
 
     return args
@@ -297,12 +411,12 @@ def _set_random_seed(seed=0, cuda_deterministic=True):
     torch.cuda.manual_seed_all(seed)
 
     if cuda_deterministic:  # slower, but more reproducible
-        torch.backends.cudnn.deterministic = True  # 固定内部随机性
         torch.backends.cudnn.enabled = False
+        torch.backends.cudnn.deterministic = True  # 固定内部随机性
         torch.backends.cudnn.benchmark = False
     else:
-        torch.backends.cudnn.deterministic = False
         torch.backends.cudnn.enabled = True
+        torch.backends.cudnn.deterministic = False
         torch.backends.cudnn.benchmark = True  # 输入尺寸一致，加速训练
 
 
@@ -324,6 +438,7 @@ def main(args):
                       config=config,
                       seed=args.seed)
     trainer.train()
+    logging.shutdown()
 
 
 if __name__ == "__main__":
