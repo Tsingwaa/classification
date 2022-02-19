cdd57903071fa487d3133132a0269172580644a1
waa <zengchh3@gmail.com>
2022-02-19 17:43:51 +0800

    update

--- scripts/ml/train_supcon.sh
+++ scripts/ml/train_supcon.sh
@@ -9,14 +9,14 @@ export PYTHONPATH=$HOME/Projects/classification
 #     torchrun --nproc_per_node="$2"  --master_port 60000 train_supcon.py \
 #     --config_path "configs/Skin7/r50pre_supcon.yaml"
 
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES="$1" \
-    torchrun --nproc_per_node="$2"  --master_port 60000 train_supcon.py \
-    --config_path "configs/Skin7/r50pre_simclr.yaml"
+# OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES="$1" \
+#     torchrun --nproc_per_node="$2"  --master_port 60000 train_supcon.py \
+#     --config_path "configs/Skin7/r50pre_simclr.yaml"
 
 # OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES="$1" \
 #     torchrun --nproc_per_node="$2"  --master_port 60000 train_supcon.py \
 #     --config_path "configs/Xray9/r50pre_supcon.yaml"
 
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES="$1" \
-    torchrun --nproc_per_node="$2"  --master_port 60000 train_supcon.py \
-    --config_path "configs/Xray9/r50pre_simclr.yaml"
+# OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES="$1" \
+#     torchrun --nproc_per_node="$2"  --master_port 60000 train_supcon.py \
+#     --config_path "configs/Xray9/r50pre_simclr.yaml"
