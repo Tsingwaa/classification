3b3eeacdd3fb1859a5f049407fa230b887ec5137
waa <zengchh3@gmail.com>
2022-02-21 17:06:23 +0800

    update

--- scripts/ml/train_ce.sh
+++ scripts/ml/train_ce.sh
@@ -7,9 +7,11 @@ export PYTHONPATH=$HOME/Projects/classification
 # Single GPU
 # CUDA_VISIBLE_DEVICES="$2" python3 train_ce.py --config_path "$1" # --lr "$3" --wd "$4"
 
+CUDA_VISIBLE_DEVICES="$1" python3 train_ce.py --config_path configs/PathMNIST/r50pre_simclr.yaml
+# CUDA_VISIBLE_DEVICES="$1" python3 train_ce.py --config_path configs/PathMNIST/r50pre_supcon.yaml
 # CUDA_VISIBLE_DEVICES="$1" python3 train_ce.py --config_path configs/PathMNIST/r32_simclr.yaml
 # CUDA_VISIBLE_DEVICES="$1" python3 train_ce.py --config_path configs/PathMNIST/r32_supcon.yaml
 # CUDA_VISIBLE_DEVICES="$1" python3 train_ce.py --config_path configs/Skin7/r50pre_simclr.yaml
 # CUDA_VISIBLE_DEVICES="$1" python3 train_ce.py --config_path configs/Skin7/r50pre_supcon.yaml
-CUDA_VISIBLE_DEVICES="$1" python3 train_ce.py --config_path configs/Xray9/r50pre_simclr.yaml
+# CUDA_VISIBLE_DEVICES="$1" python3 train_ce.py --config_path configs/Xray9/r50pre_simclr.yaml
 # CUDA_VISIBLE_DEVICES="$1" python3 train_ce.py --config_path configs/Xray9/r50pre_supcon.yaml
